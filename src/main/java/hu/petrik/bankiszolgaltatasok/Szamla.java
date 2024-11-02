10dc92ccb30891bd161e165cc9cda00bd860627e
Peti <nemt451@gmail.com>
2024-11-02 12:41:05 +0100

    Szamla osztály

---
+++ src/main/java/hu/petrik/bankiszolgaltatasok/Szamla.java
@@ -0,0 +1,23 @@
+package hu.petrik.bankiszolgaltatasok;
+
+public class Szamla extends BankiSzolgaltatas {
+    private double egyenleg;
+
+    public Szamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+        this.egyenleg = 0.0;
+    }
+
+    public double getEgyenleg() {
+        return egyenleg;
+    }
+
+    public void befizet(double osszeg) {
+        this.egyenleg += osszeg;
+    }
+
+    public boolean kivesz(double osszeg) {
+        // Nem implementált metódus
+        return false;
+    }
+}
