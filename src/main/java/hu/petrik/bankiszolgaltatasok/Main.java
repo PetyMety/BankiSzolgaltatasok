7feee3f964b73a90d664ef4d29645c8f9a7113fa
Peti <nemt451@gmail.com>
2024-11-02 12:53:18 +0100

    Main - Példa

--- src/main/java/hu/petrik/bankiszolgaltatasok/Main.java
+++ src/main/java/hu/petrik/bankiszolgaltatasok/Main.java
@@ -2,6 +2,23 @@ package hu.petrik.bankiszolgaltatasok;
 
 public class Main {
     public static void main(String[] args) {
+        Tulajdonos tulajdonos = new Tulajdonos("John Doe");
+        Bank bank = new Bank();
 
+        Szamla szamla1 = bank.szamlaNyitas(tulajdonos, 1000.0);
+        Szamla szamla2 = bank.szamlaNyitas(tulajdonos, 0.0);
+
+        szamla1.befizet(1000.0);
+        szamla2.befizet(500.0);
+
+        Kartya kartya1 = szamla1.ujKarta("1234-5678-9012-3456");
+        Kartya kartya2 = szamla2.ujKarta("9876-5432-1098-7654");
+
+        kartya1.vasarlas(200.0);
+        kartya2.vasarlas(300.0);
+
+        System.out.println("Össz egyenleg: " + bank.getOsszEgyenleg(tulajdonos));
+        System.out.println("Legnagyobb egyenlegű számla: " + bank.getLegnagyobbEgyenleguSzamla(tulajdonos));
+        System.out.println("Össz hitelkeret: " + bank.getOsszHitelkeret());
     }
 }
