669a42385b1ee0ed1937f46a5629b8c48d2fc337
Peti <nemt451@gmail.com>
2024-11-02 12:33:02 +0100

    Tulajdonos

---
+++ src/main/java/hu/petrik/bankiszolgaltatasok/Tulajdonos.java
@@ -0,0 +1,13 @@
+package hu.petrik.bankiszolgaltatasok;
+
+public class Tulajdonos {
+    private String nev;
+
+    public Tulajdonos(String nev) {
+        this.nev = nev;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+}
